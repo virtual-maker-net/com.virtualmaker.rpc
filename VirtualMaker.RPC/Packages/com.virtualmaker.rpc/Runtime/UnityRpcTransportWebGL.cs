fb1a8bc3492c4fcd7c657978fd447047eaab9aa0
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-04-23 20:52:42 -0400

    com.virtualmaker.rpc 1.0.0-preview.1 (#1)

---
+++ VirtualMaker.RPC/Packages/com.virtualmaker.rpc/Runtime/UnityRpcTransportWebGL.cs
@@ -0,0 +1,49 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_WEBGL
+
+using AOT;
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+
+namespace VirtualMaker.RPC
+{
+    internal class UnityRpcTransportWebGL : IUnityRpcTransport
+    {
+        private static UnityRpcTransportWebGL _instance;
+
+        public ConcurrentQueue<string> ReceiveQueue { get; }
+
+        [DllImport("__Internal")]
+        private static extern void UnityRpcTransportWebGLCreate(Action<string> onMessage);
+
+        [DllImport("__Internal")]
+        private static extern void UnityRpcTransportWebGLSend(string message);
+
+        public UnityRpcTransportWebGL()
+        {
+            _instance = this;
+            ReceiveQueue = new ConcurrentQueue<string>();
+            UnityRpcTransportWebGLCreate(OnMessage);
+        }
+
+        [MonoPInvokeCallback(typeof(Action<string>))]
+        public static void OnMessage(string message)
+        {
+            _instance.OnMessageInstance(message);
+        }
+
+        public void SendMessage(string message)
+        {
+            UnityRpcTransportWebGLSend(message);
+        }
+
+        private void OnMessageInstance(string message)
+        {
+            ReceiveQueue.Enqueue(message);
+        }
+    }
+}
+
+#endif // UNITY_WEBGL
